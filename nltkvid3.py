e0b8e924e034975c47d33a7e1e2786d18e0c7105
Nilesh Khatri <da201712099@gmail.com>
2019-04-04 11:47:51 +0530

    Update nltkvid3.py

--- nltkvid3.py
+++ nltkvid3.py
@@ -5,8 +5,8 @@ ps = PorterStemmer()
 
 example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
 
-##for w in example_words:
-##    print(ps.stem(w))
+for w in example_words:
+   print(ps.stem(w))
 
 
 new_text = "It is very important to be pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
